b04c39fa5d18cdec240a6ca5eb2ac1995989e45e
1Ir-is <mhuynk1005@gmail.com>
2024-04-16 16:48:16 +0700

    feat: fix the talkjs when user is null

--- src/Components/ChatBox/TalkJs.jsx
+++ src/Components/ChatBox/TalkJs.jsx
@@ -7,38 +7,41 @@ function TalkJs() {
     const { user } = useAuth();
     console.log(user);
     useEffect(() => {
-        Talk.ready.then(() => {
-            const me = new Talk.User({
-                id: user.userId.toString(), // Replace with your user ID
-                name: user.name, // Replace with your name
-                email: user.email, // Replace with your email
-                photoUrl: user.avatar, // Replace with your photo URL
-                welcomeMessage: 'Hey there! How can I help you?', // Optional welcome message
-                role: 'user'
+        if (user) {
+            Talk.ready.then(() => {
+                const me = new Talk.User({
+                    id: user.userId.toString(), // Replace with your user ID
+                    name: user.name, // Replace with your name
+                    email: user.email, // Replace with your email
+                    photoUrl: user.avatar, // Replace with your photo URL
+                    welcomeMessage: 'Hey there! How can I help you?', // Optional welcome message
+                    role: 'user'
+                });
+    
+                const admin = new Talk.User({
+                    id: '20', // Replace with your admin user ID
+                    name: 'Admin', // Replace with admin name
+                    email: 'admin@gmail.com', // Replace with admin email
+                    photoUrl: 'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg', // Replace with admin photo URL
+                    role: 'admin'
+                    // Optional welcome message for admin
+                });
+    
+                var session = new Talk.Session({
+                    appId: 'tcKEJWWb',
+                    me: me,
+                });
+    
+                var conversation = session.getOrCreateConversation(Talk.oneOnOneId(me, admin));
+                conversation.setParticipant(me);
+                conversation.setParticipant(admin);
+    
+                var inbox = session.createInbox({ selected: conversation });
+                inbox.mount(document.getElementById('talkjs-container'));
             });
-
-            const admin = new Talk.User({
-                id: '20', // Replace with your user ID
-                name: 'Admin', // Replace with your name
-                email: 'admin@gmail.com', // Replace with your email
-                photoUrl: 'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg',
-                role: 'admin'
-                // Replace  // Optional welcome message
-            });
-
-            var session = new Talk.Session({
-                appId: 'tcKEJWWb',
-                me: me,
-            });
-
-            var conversation = session.getOrCreateConversation(Talk.oneOnOneId(me, admin));
-            conversation.setParticipant(me);
-            conversation.setParticipant(admin);
-
-            var inbox = session.createInbox({ selected: conversation });
-            inbox.mount(document.getElementById('talkjs-container'));
-        });
+        }
     }, [user]);
+    
 
     return <div id="talkjs-container" style={{ height: '500px' }}></div>;
 }
